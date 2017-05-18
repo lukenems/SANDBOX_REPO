36eebdf0819fabe3761839f6820a7b45f06345bb
Luke Nemy <admin@Admins-MacBook-Pro-10.local>
2017-05-17 18:15:14 -0700

    Albums rendered on Individual Artist pages, Server.js is refactored & paths re-routed, added styling,

--- Web_Servers/src/server.js
+++ Web_Servers/src/server.js
@@ -3,67 +3,73 @@
 var express = require('express');
 var fs = require('fs');
 var path = require('path');
-//var home = require('./views/index.ejs');
-//var albums = require('./views/albums.ejs');
-//var songs = require('./views/songs.ejs');
-//var artists = require('./views/artists.ejs');
-//var album = require('./views/album.ejs');
+var albums = require('../albums.json');
+var artists = require('../artists.json');
+var songs = require('../songs.json');
+
+function albumsForArtist(artistId){
+  var albumArr = []
+  var artistIds = parseInt(artistId) + 1;
+  for (var i = 0; i < albums.length; i++) {
+    if(artistIds === albums[i].artist_id){
+      albumArr.push(albums[i])
+    }
+  }
+  return albumArr
+}
 
 var app = express();
 
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-  fs.readFile('artists.json', 'utf8', (err, artists) => {
-    if (err) throw err;
-    artists = JSON.parse(artists)
+
     console.log("these are the ", typeof artists)
     res.render('index', {
       artists: artists,
     })
 })
-})
 
-app.get('/albums/:id?', function(req, res){
-  // console.log(req.params)
-  // var album_id = req.params.album_id;
-  // var album = albums[album_id];
-  fs.readFile('albums.json', 'utf8', (err, albums) => {
-    if (err) throw err;
-    albums = JSON.parse(albums)
+
+app.get('/albums', function(req, res){
+
     console.log("these are the ", typeof albums)
     res.render('albums', {
       albums: albums,
     })
 })
+
+
+app.get('/albums/:id', function(req, res){
+  var id = req.params.id;
+    console.log("these are the ", typeof albums)
+    res.render('album', {
+      albums: albums[id],
+    })
 })
 
 app.get('/songs', function(req, res){
-  console.log(path.isAbsolute(__dirname))
-  //read songs.json
-  //parse songs json
-  fs.readFile('songs.json', 'utf8', (err, songs) => {
-    if (err) throw err;
-    songs = JSON.parse(songs)
-    console.log("these are the ", typeof songs)
     res.render('songs', {
       songs: songs,
     })
 
   });
- })
 
-app.get('/artists/:artist_id?', function(req, res){
-  //var artist_id = req.params.artist_id;
-  //var artist = artists[artist_id];
-  fs.readFile('artists.json', 'utf8', (err, artists) => {
-    if (err) throw err;
-    artists = JSON.parse(artists)
-    console.log("these are the ", typeof artists)
-    res.render('artists', {
+app.get('/artists', function(req, res){
+    res.render('index', {
       artists: artists,
     })
 })
+
+app.get('/artists/:id', function(req, res){
+  var id = req.params.id;
+  var artistAlbums = albumsForArtist(id)
+  console.log(artistAlbums);
+    res.render('artist', {
+      artist: artists[id],
+      album: artistAlbums,
+    })
+
 })
 
 app.listen(3000, function() {
