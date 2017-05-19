5cca7a51db60200b4078672d2662ef1217043699
Luke Nemy <admin@Admins-MacBook-Pro-10.local>
2017-05-18 18:13:35 -0700

    "Links to Artist and album pages from respective pages to standalone pages, figured out SongCount functionality, more bootstrap styling"

--- Web_Servers/src/server.js
+++ Web_Servers/src/server.js
@@ -7,24 +7,51 @@ var albums = require('../albums.json');
 var artists = require('../artists.json');
 var songs = require('../songs.json');
 
+//finds albums in albums.json by artist_id
 function albumsForArtist(artistId){
+  //array of albums for individual artist
   var albumArr = []
-  var artistIds = parseInt(artistId) + 1;
+  //loops through albums.json. if the id from the URL matches artist_id, push that
+  //that album into individual artist albums array
   for (var i = 0; i < albums.length; i++) {
-    if(artistIds === albums[i].artist_id){
+    if(artistId === albums[i].artist_id){
       albumArr.push(albums[i])
     }
   }
   return albumArr
 }
 
+function artistForAlbum(albumId) {
+  var artistArr = []
+
+  for (var i = 0; i < artists.length; i++) {
+    if (albumId === albums[i].artist_id) {
+      artistArr.push(albums[i])
+    }
+  }
+  console.log('artist array' + artistArr)
+  return artistArr
+}
+
+function songsForAlbum(artistId){
+  //array of songs for individual artist
+  var songsArr = []
+  //loops through songs and pushes songs equal to album_id into songsArr
+  for (var i = 0; i < songs.length; i++) {
+    if(artistId === songs[i].album_id){
+      songsArr.push(songs[i])
+    }
+  }
+  //returns the total song count of album_id
+  return songsArr.length
+}
+
+
 var app = express();
 
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-
-    console.log("these are the ", typeof artists)
     res.render('index', {
       artists: artists,
     })
@@ -32,28 +59,27 @@ app.get('/', function(req, res) {
 
 
 app.get('/albums', function(req, res){
-
-    console.log("these are the ", typeof albums)
     res.render('albums', {
       albums: albums,
+      artists: artists,
+      songs: songs,
     })
 })
 
 
 app.get('/albums/:id', function(req, res){
-  var id = req.params.id;
-    console.log("these are the ", typeof albums)
+  var id = parseInt(req.params.id) - 1;
+  var artistName = artistForAlbum(id)
     res.render('album', {
       albums: albums[id],
-    })
-})
-
-app.get('/songs', function(req, res){
-    res.render('songs', {
+      albums: albums,
+      artist: artistName,
+      artists: artists,
       songs: songs,
     })
+    console.log(artists);
+})
 
-  });
 
 app.get('/artists', function(req, res){
     res.render('index', {
@@ -62,15 +88,31 @@ app.get('/artists', function(req, res){
 })
 
 app.get('/artists/:id', function(req, res){
-  var id = req.params.id;
+  //converts req.params.id into an integer so it can be passed into artist[id]
+  var id = parseInt(req.params.id);
+  //assigns artistAlbums to artist_id
   var artistAlbums = albumsForArtist(id)
-  console.log(artistAlbums);
+  //array for all individual artist albums
+
+  var albumSongs = []
+  for (var i=0; i < artistAlbums.length; i++) {
+    // albumSongs is using both functions
+    albumSongs.push(songsForAlbum(artistAlbums[i].id))
+  }
     res.render('artist', {
-      artist: artists[id],
+      //makes the index of artists begin at 1 instead of 0
+      artist: artists[id - 1],
       album: artistAlbums,
+      songCount: albumSongs,
     })
 
 })
+
+app.get('/songs', function(req, res){
+  res.render('songs', {
+    songs: songs,
+  })
+});
 
 app.listen(3000, function() {
   console.log("The frontend server is running port 3000 just fine!");
